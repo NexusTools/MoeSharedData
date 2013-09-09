8ef2262ef0986e17797bd5140d5c31d62c6ccbc0
Katelyn <ktaeyln@gmail.com>
2013-09-08 21:19:20 -0400

    Cube zoom effect

--- examples/opengl/init.js
+++ examples/opengl/init.js
@@ -5,7 +5,7 @@ if(!surface.hasOpenGL())
     throw "OpenGL Support Required: " + surface.lastOpenGLError();
 
 var glView = new GLGraphicsView(surface);
-glView.camZ = -8;
+glView.camZ = -200;
 
 var glScene = new GLScene();
 glView.setScene(glScene);
@@ -13,10 +13,15 @@ glView.setScene(glScene);
 var glCube = new GLCubeModel();
 glScene.addModel(glCube);
 
-
-engine.tick.connect(function() {
-    glCube.rotate(0, 1, 0);
-    glView.update();
+surface.connected.connect(function() {
+    engine.tick.connect(function() {
+        glCube.rotate(0, 1, 0.2);
+        if(glView.camZ < -8) {
+            glView.camZ += (-7.9 - glView.camZ)/24;
+            if(glView.camZ > -8)
+                glView.camZ = -8;
+        }
+    });
 });
 
 surface.resized.connect(glView.setSize);
